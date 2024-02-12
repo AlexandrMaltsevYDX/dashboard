e58b0c2ec68028c08cb9eaf2257de2cefb369e3f
AlexandrMaltsevYDX <AlexandrMaltsve@yandex.ru>
2024-02-12 20:59:29 +0100

    fix slug name

--- apps/reobjects/serializers/objects_re/reobject_mtm_enjineeringservice.py
+++ apps/reobjects/serializers/objects_re/reobject_mtm_enjineeringservice.py
@@ -9,7 +9,7 @@ from apps.reobjects import models, serializers
 
 
 class ReObjectEngineeringServicesModelSerializer(ModelSerializer):
-    engineering_service = SlugRelatedField(read_only=True, slug_field="value")
+    engineering_service = SlugRelatedField(read_only=True, slug_field="name")
 
     class Meta:
         model = models.objects_re.ReObjectEngineeringServices
