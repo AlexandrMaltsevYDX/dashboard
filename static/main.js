4ab627709e8fdfecce8d401d90e2eb5eecd9a7af
AlexandrMaltsevYDX <AlexandrMaltsve@yandex.ru>
2024-02-23 16:40:29 +0100

    fix bags with select

--- static/main.js
+++ static/main.js
@@ -3,12 +3,25 @@ document.addEventListener("DOMContentLoaded", function (event) {
     setTimeout(() => {
         console.log("Delayed for 1 second.");
         const selects = document.querySelectorAll(".select2-hidden-accessible")
+        const navitems = document.querySelectorAll(".nav-item")
+        const addRows = document.querySelectorAll(".add-row")
         console.log(selects)
         selects.forEach((e) => test(e))
-    }, 1500);
+        navitems.forEach((e) => addEvents(e))
+        addRows.forEach((e) => addEvents(e))
+    }, 1000);
 });
 
 
 function test(elem) {
     elem.classList.remove("select2-hidden-accessible")
+}
+
+function addEvents(elem) {
+    elem.addEventListener("click", deleteSelect2)
+}
+
+function deleteSelect2() {
+    const selects = document.querySelectorAll(".select2-hidden-accessible")
+    selects.forEach((e) => test(e))
 }
