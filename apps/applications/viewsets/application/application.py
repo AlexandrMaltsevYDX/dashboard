cde8fc4487c52b4b6a3d5bc199f7140e02b7fcc6
AlexandrMaltsevYDX <AlexandrMaltsve@yandex.ru>
2024-06-11 15:15:26 +0200

    add backend save changes

--- apps/applications/viewsets/application/application.py
+++ apps/applications/viewsets/application/application.py
@@ -37,7 +37,7 @@ class ApplicationModelViewSet(
     def perform_create(self, serializer):
         super().perform_create(serializer)
         tasks.send_email.delay(
-            subject="Заявка",
+            subject="Заявка с сайта Загородный стиль",
             message=f"""
             {serializer.data['text'] }
             {serializer.data['applicant'] }
